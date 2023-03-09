8512c952e04674173414ed8e685faf6a1713fb5e
Igor Kochugin <zero-jpg@inbox.ru>
2023-03-09 10:46:57 +0300

    Рефактринг, добавление прогресс бара, обощение путей к файлам

--- programm/__init__.py
+++ programm/__init__.py
@@ -11,9 +11,14 @@ from PIL import Image, ImageFilter
 from langdetect import detect
 import pyperclip
 import sys
+import pathlib
+from pathlib import Path
+from progress.bar import IncrementalBar
 
 pyautogui.FAILSAFE = False
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+user_path_IGOR = "SmartTesterForBoas"
+user_path_LERA = "Smart_tester_py"
 Column_Coords = [0, 0]
 
 # def leftClick(coord):
@@ -25,10 +30,10 @@ Column_Coords = [0, 0]
 def lookOnScreen(lang: str, is_obrez: bool):
     # поиск слова на экране
     # 1018 294  1205 536
-    temp_screen = pyautogui.screenshot('temp.bmp')
+    temp_screen = pyautogui.screenshot(r'..\images\temp.bmp')
     img_seryy = temp_screen.convert("L")# .save(r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
     # temp_kraya = img_seryy.filter(ImageFilter.FIND_EDGES)#.save(r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
-    img_seryy.filter(ImageFilter.DETAIL).save(r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
+    img_seryy.filter(ImageFilter.DETAIL).save(r"..\images\temp_kraya.bmp")
     # temp_kraya = img_seryy.filter(ImageFilter.FIND_EDGES).save(r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
     # temp_kraya = img_seryy.filter(ImageFilter.SMOOTH)
     # temp_kraya = temp_kraya.filter(ImageFilter.EMBOSS).save(r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
@@ -43,11 +48,11 @@ def lookOnScreen(lang: str, is_obrez: bool):
     # получить все данные из изображения
     # temp_kraya = temp_kraya.filter(ImageFilter.MaxFilter(1)).save(
     #    r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp')
-    print(lang)
-    link = r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya.bmp'
-    link_obrez = r'C:\Users\drtar\Desktop\SmartTesterForBoas\programm\temp_kraya_obrez.bmp'
+    #print(lang)
+    link = r"..\images\temp_kraya.bmp"
+    link_obrez = r"..\images\temp_kraya_obrez.bmp"
     if is_obrez:
-        print(Column_Coords)
+        #print(Column_Coords)
         temp_kraya = Image.open(link)
         im_crop = temp_kraya.crop((Column_Coords[0], 0, Column_Coords[1], GetSystemMetrics(1)))
         im_crop.save(link_obrez)
@@ -61,16 +66,16 @@ def lookOnScreen(lang: str, is_obrez: bool):
     else:
         data = pytesseract.image_to_data(l, lang=lang, config='--oem 3 --psm 12',
                                          output_type=pytesseract.Output.DICT)
-    print(data)
+    #print(data)
 
     return data
 
-def listWordSearch():
-    temp_screen = pyautogui.screenshot('temp.bmp')
-    img_seryy = temp_screen.convert("L")
-    list_star = pyautogui.locateCenterOnScreen(r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\list_start.bmp')
-    print(list_star)
-    pyautogui.leftClick(list_star)
+# def listWordSearch():
+#     temp_screen = pyautogui.screenshot('temp.bmp')
+#     img_seryy = temp_screen.convert("L")
+#     list_star = pyautogui.locateCenterOnScreen(r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\list_start.bmp')
+#     print(list_star)
+#     pyautogui.leftClick(list_star)
 
 
 def centerWordSearch(word: str, lang: str, cut: bool, screen_shot = 'nothing'):
@@ -81,7 +86,7 @@ def centerWordSearch(word: str, lang: str, cut: bool, screen_shot = 'nothing'):
         data = lookOnScreen(lang, cut)
     else:
         data = pytesseract.image_to_data(screen_shot, lang=lang, config='--oem 3 --psm 12', output_type=pytesseract.Output.DICT)
-        print(data)
+        #print(data)
     # получение координат
     #print("искомое слово:" + word)
     #print(data["text"])
@@ -89,11 +94,11 @@ def centerWordSearch(word: str, lang: str, cut: bool, screen_shot = 'nothing'):
     for d in data["text"]:
         if d.find(word) != -1:
             #print(word)
-            print(d)
+            #print(d)
             word_id = data["text"].index(d)
             x = data["left"][word_id] + data["width"][word_id] / 2
             y = data["top"][word_id] + data["height"][word_id] / 2
-            print("["+str(x)+","+str(y)+"]")
+            #print("["+str(x)+","+str(y)+"]")
     if cut:
         return [int(x)+Column_Coords[0], int(y)]
     else:
@@ -114,7 +119,7 @@ def neerestFigure(link: str, coords: list):
         if d < minDist and coords[0]<pos[0]:
             imageCoords = [pos[0], pos[1]]
             minDist = d
-    print(imageCoords)
+    #print(imageCoords)
     return imageCoords
 
 def neerestWord(word_main: str, word_sub: str, lang: str):
@@ -139,42 +144,42 @@ def neerestWord(word_main: str, word_sub: str, lang: str):
 def findWordWithPicrure(skrin_location: str, word: str, lang: str):
     screenshot_pos = pyautogui.locateOnScreen(skrin_location)
     word_center = centerWordSearch(word, lang, False, skrin_location)
-    print("Поиск со скриншотом. Отладка:")
-    print(word_center)
-    print(screenshot_pos)
-    print([word_center[0]+screenshot_pos.left, word_center[1]+screenshot_pos.top])
+    #print("Поиск со скриншотом. Отладка:")
+    #print(word_center)
+    #print(screenshot_pos)
+    #print([word_center[0]+screenshot_pos.left, word_center[1]+screenshot_pos.top])
     return [word_center[0]+screenshot_pos.left, word_center[1]+screenshot_pos.top]
 
 
 # Забираем данные для прохода по полям
 data = pd.DataFrame(pd.read_excel(
-    io=r'C:\Users\drtar\Desktop\SmartTesterForBoas\Копия (Агрегатор Т1) ТК для тестирования интеграции между ELMA и IMOS (3).xlsx',
+    io="..\Копия (Агрегатор Т1) ТК для тестирования интеграции между ELMA и IMOS (3).xlsx",
     engine='openpyxl'))
 out_tab = data.copy()
-frame = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\frame.bmp'
-full_frame = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\full_frame.bmp'
-ok_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\ok_button.bmp'
-error_wind = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\error_wind.bmp'
-active_ok_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\active_ok_button.bmp'
-active_ok_button_error = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\active_ok_button_error.bmp'
-text_field = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\text_field.bmp'
-use_config_wind = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\use_config_wind.bmp'
-active_yes_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\active_yes_button.bmp'
-show_spec_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\show_spec_button.bmp'
-one_c_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\one_c_button.bmp'
-exit_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\exit_button.bmp'
-config_set_button = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\config_set_button.bmp'
-ok_cancel_toolbar = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\ok_cancel_toolbar.bmp'
-error_wind_color = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\error_wind_color.bmp'
-window_color_1 = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\window_color_1.bmp'
-window_color_2 = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\window_color_2.bmp'
-window_color_3 = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\window_color_3.bmp'
-error_logo = r'C:\Users\drtar\Desktop\SmartTesterForBoas\images\error_logo.bmp'
+frame = r'..\images\frame.bmp'
+full_frame = r'..\images\full_frame.bmp'
+ok_button = r'..\images\ok_button.bmp'
+error_wind = r'..\images\error_wind.bmp'
+active_ok_button = r'..\images\active_ok_button.bmp'
+active_ok_button_error = r'..\images\active_ok_button_error.bmp'
+text_field = r'..\images\text_field.bmp'
+use_config_wind = r'..\images\use_config_wind.bmp'
+active_yes_button = r'..\images\active_yes_button.bmp'
+show_spec_button = r'..\images\show_spec_button.bmp'
+one_c_button = r'..\images\one_c_button.bmp'
+exit_button = r'..\images\exit_button.bmp'
+config_set_button = r'..\images\config_set_button.bmp'
+ok_cancel_toolbar = r'..\images\ok_cancel_toolbar.bmp'
+error_wind_color = r'..\images\error_wind_color.bmp'
+window_color_1 = r'..\images\window_color_1.bmp'
+window_color_2 = r'..\images\window_color_2.bmp'
+window_color_3 = r'..\images\window_color_3.bmp'
+error_logo = r'..\images\error_logo.bmp'
 
 # print(data.columns[0])
 colum_name = []
 columns_count = data.shape[1]
-print(data.shape[0])
+#print(data.shape[0])
 rows_count = data.shape[0]
 for index in range(columns_count):
     colum_name.append(data.columns[index].split(' '))
@@ -184,7 +189,9 @@ out_tab["Ссылка на json"] = ["Отсутсвует"]*rows_count
 # print(colum_name[0][1])
 #pyautogui.leftClick(neerestWord(colum_name[0][0], colum_name[0][1]))
 
+mylist = [1, 2, 3, 4, 5, 6, 7]
 for index, row in data.iterrows():
+    bar = IncrementalBar("Тест-кейс " + str(index+1), max=len(mylist))
     # try:
     # Нахождение названия свойства и элемента со списком
     # print(row[0] + '|' + row[1])
@@ -200,15 +207,15 @@ for index, row in data.iterrows():
     else:
         pyautogui.leftClick(centerWordSearch(row[0], 'eng', True))
     pyautogui.sleep(.2)
-    print(colum_name[1][0] + '|' + colum_name[1][1])
+    #print(colum_name[1][0] + '|' + colum_name[1][1])
     second_name = neerestWord(colum_name[1][0], colum_name[1][1], 'rus')
     pyautogui.leftClick(neerestFigure(frame, second_name))
-    print("ОТЛАДКА!!!!")
+    #print("ОТЛАДКА!!!!")
     # Нахождение нужного значения
     for index_value in range(columns_count-1):
         try:
             index_value += 1
-            print(row[index_value] + detect(row[index_value]))
+            #print(row[index_value] + detect(row[index_value]))
             if len(row[index_value].split(" ")) == 1:
                 if detect(row[index_value]) == 'ru' or detect(row[index_value]) == 'uk' or detect(row[index_value]) == 'bk' \
                         or detect(row[index_value]) == 'bg' or detect(row[index_value]) == 'mk':
@@ -224,6 +231,7 @@ for index, row in data.iterrows():
                 # elif detect(row[0]) == 'en' or detect(row[0]) == 'cy':
                 else:
                     pyautogui.leftClick(neerestWord(words[0], words[1], 'eng'))
+            bar.next()
             pyautogui.sleep(.2)
             pyautogui.leftClick(neerestWord("Дополнительные", "параметры", 'rus'))
             # temp = findWordWithPicrure(ok_cancel_toolbar, "OK", 'eng')
@@ -245,6 +253,7 @@ for index, row in data.iterrows():
             pyautogui.keyUp('ctrl')
             pyautogui.write(datetime.datetime.now().strftime('%H:%M:%S.%f')) #2023-03-07|2023-03-0714:04:06.055323
             pyautogui.leftClick(centerWordSearch("OK", 'eng', False))
+            bar.next()
             pyautogui.sleep(1)
             if pyautogui.locateCenterOnScreen(use_config_wind) != None:
                 pyautogui.leftClick(pyautogui.locateCenterOnScreen(active_yes_button))# findWordWithPicrure(use_config_wind, "Да", 'rus'))
@@ -254,10 +263,10 @@ for index, row in data.iterrows():
                 pyautogui.sleep(1)
                 Column_Coords[0] = pyautogui.locateOnScreen(window_color_1)['left']
                 Column_Coords[1] = pyautogui.locateOnScreen(window_color_1)['top']
-                print("Отладочка:")
+                #print("Отладочка:")
                 color_cord = centerWordSearch("Белый", 'rus', False, window_color_1)
                 pyautogui.sleep(.2)
-                print(color_cord)
+                #print(color_cord)
                 if color_cord == [0, -7]:
                     Column_Coords[0] = pyautogui.locateOnScreen(window_color_2)['left']
                     Column_Coords[1] = pyautogui.locateOnScreen(window_color_2)['top']
@@ -274,26 +283,32 @@ for index, row in data.iterrows():
                     pyautogui.leftClick(color_cord)
                     pyautogui.sleep(.2)
             pyautogui.leftClick(pyautogui.locateCenterOnScreen(show_spec_button))
+            bar.next()
             pyautogui.sleep(.5)
             # pyautogui.leftClick(centerWordSearch("OK", 'eng', False))
             # pyautogui.sleep(.2)
             pyautogui.leftClick(pyautogui.locateCenterOnScreen(one_c_button))
+            bar.next()
             pyautogui.sleep(1)
+            bar.next()
             pyautogui.leftClick(centerWordSearch("OK", 'eng', False))
             pyautogui.sleep(.2)
+            bar.next()
             pyautogui.leftClick(pyautogui.locateCenterOnScreen(exit_button))
             pyautogui.sleep(.2)
             # pyautogui.leftClick(pyautogui.locateCenterOnScreen(exit_button))
             # pyautogui.sleep(.2)
             out_tab["Статус"][index_value] = "Succes"
             out_tab["Ссылка на json"][index_value] = pyperclip.paste()
+            bar.next()
             pyautogui.leftClick(pyautogui.locateCenterOnScreen(config_set_button))
             pyautogui.sleep(2)
+            bar.finish()
         except Exception:
             e = sys.exc_info()[1]
             out_tab["Статус"][index_value] = e.args[0]
         # create excel writer
-        writer = pd.ExcelWriter(r'C:\Users\drtar\Desktop\SmartTesterForBoas\logs.xlsx')
+        writer = pd.ExcelWriter(r'..\logs.xlsx')
         # write dataframe to excel sheet named 'marks'
         out_tab.to_excel(writer, 'marks')
         # save the excel file
